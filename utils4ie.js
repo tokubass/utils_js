f645b5649298d8e16e444c1c99f83a423f9582bf
yasushi hashimoto <hashimoto@seesaa.co.jp>
2013-06-05 20:02:35 +0900

    update

---
+++ utils4ie.js
@@ -0,0 +1,17 @@
+if( typeof document.getElementsByClassName === "undefined"){
+    document.getElementsByClassName = function(class_name){
+	var  class_selectors, _element, _elements, i, _len;
+	class_selectors = [];
+	_elements = document.getElementsByTagName("*");
+	_len = _elements.length;
+	if(_len > 0){
+	    for( i = 0; i < _len; i++){
+		_element = _elements[i];
+		if(_element.getAttribute("className") === class_name){
+		    class_selectors.push( _element);
+		}
+	    }
+	}
+	return class_selectors;
+    };
+}
